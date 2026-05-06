d3aa8d11041e8cdf0edb06f4e96dfff6dc42a171
cognosales <cognosales@gmail.com>
2026-05-06 09:03:00 -0700

    Update contact.functions.server.ts

--- src/lib/contact.functions.server.ts
+++ src/lib/contact.functions.server.ts
@@ -1,4 +1,4 @@
-import { createServerFn } from "@tanstack/react-start/server";
+import { createServerFn } from "@tanstack/react-start";
 import { z } from "zod";
 import { supabaseAdmin } from "@/integrations/supabase/client.server";
 
