e6482493b9ff3670fff3a4b22a2cad648752fb4f
cognosales <cognosales@gmail.com>
2026-05-06 10:47:38 -0700

    Update entry.server.ts

--- src/entry.server.ts
+++ src/entry.server.ts
@@ -1,6 +1,3 @@
 import { createStartHandler } from '@tanstack/react-start/server'
-import { getRouterManifest } from '@tanstack/react-start/router-manifest'
 
-export default createStartHandler({
-  getRouterManifest,
-})
+export default createStartHandler()
