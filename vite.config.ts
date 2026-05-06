57f820bb529f7dcfdec0a89566d183e94d8389a7
cognosales <cognosales@gmail.com>
2026-05-06 10:33:49 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
-
+import { defineConfig } from '@lovable.dev/vite-tanstack-config'
+ 
 export default defineConfig({
   cloudflare: false,
   tanstackStart: {
-    target: "node-server",
+    target: 'node-server',
+    entry: 'src/entry.server.ts',
   },
   server: {
-    host: "0.0.0.0",
+    host: '0.0.0.0',
     port: process.env.PORT || 3000,
   },
-});
+})
